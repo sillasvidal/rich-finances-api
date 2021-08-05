f47d928e373dfb12b94e1aa8d63f9a8b9dcae6a4
Sillas Vidal <sillasv91@gmail.com>
2021-08-05 18:40:09 -0300

    Deleted non-using vars

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,5 +1,5 @@
 import cors from 'cors';
-import express, { NextFunction, Request, Response } from 'express';
+import express, { Request, Response } from 'express';
 
 import 'express-async-errors';
 import 'reflect-metadata';
@@ -16,7 +16,7 @@ app.use(express.json());
 
 app.use(routes);
 
-app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+app.use((err: Error, request: Request, response: Response) => {
   if (err instanceof AppError) {
     return response.status(err.statusCode).json({
       status: 'error',
